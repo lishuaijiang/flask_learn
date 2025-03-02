083b8c21e306fbc469265c51de725ca9db572fef
lishuaijiang <lishuaijiang@yeah.net>
2025-03-02 20:39:56 +0800

    flask use click basic structure

---
+++ app.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+# 程序启动第一时间加载环境变量，防止后续使用环境变量时，因为加载过晚导致环境变量为 None 问题
+load_dotenv()
+from learn import create_app
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run()
