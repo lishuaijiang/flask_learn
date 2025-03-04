6f8faa53bcec0a5ba28d7de0099a0e176f43f402
lishuaijiang <lishuaijiang@yeah.net>
2025-03-04 12:57:25 +0800

    flask_sqlalchemy demo

---
+++ learn/models.py
@@ -0,0 +1,10 @@
+from learn import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
